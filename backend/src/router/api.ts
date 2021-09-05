aab6e61d92ac20e2399fb5c787270ab706f88d03
Anillc <i@anillc.cn>
2021-09-05 23:35:07 +0800

    fix injecting

--- backend/src/router/api.ts
+++ backend/src/router/api.ts
@@ -34,11 +34,22 @@ router.get('/peer', async ctx => {
     ctx.body = new RestResponse(RestCode.Ok, null, ctx.user.peers)
 })
 
-//TODO: avoid injecting
+const V6_REGEX = /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/
+const V4_REGEX = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+const LINK_LOCAL_REGEX = /^fe80:(:[0-9a-fA-F]{0,4}){0,4}$/
+const DOMAIN_REGEX = /^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/
+const ASN_REGEX = /^(424242|420127)\d{4}$/
+
 router.post('/peer', async ctx => {
     const query = ctx.query as Record<string, string>
     let port: number
     if (!checkFields(query, ['server', 'address', 'port', 'asn', 'v4', 'linkLocal']) || !(port = parseInt(query.port))) {
+        ctx.body = new RestResponse(RestCode.Error, 'Invalid params')
+        return
+    }
+    const { address, asn, v4, linkLocal } = query
+    if (!(V6_REGEX.test(address) || V4_REGEX.test(address) || DOMAIN_REGEX.test(address))
+            && !V4_REGEX.test(v4) && !LINK_LOCAL_REGEX.test(linkLocal) && !ASN_REGEX.test(asn)) {
         ctx.body = new RestResponse(RestCode.Error, 'Invalid params')
         return
     }
@@ -49,7 +60,7 @@ router.post('/peer', async ctx => {
     }
     const asnEmail = await getASNEmail(query.asn)
     let email: string
-    if(query.token) {
+    if (query.token) {
         const payload = jwt.verify(query.token as string, process.env.KEY) as Record<string, string>
         email = payload.email
     } else {
